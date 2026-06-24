2fbe0de697ecd7f201ccca22306c3cd35bd54f99
ryan.limaa@outlook.com <ryan.limaa@outlook.com>
2026-06-24 09:48:45 -0300

    Criando controller de estado

---
+++ WFConFin/WFConFin/Controllers/EstadoController.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WFConFin.Data;
+using WFConFin.Models;
+
+namespace WFConFin.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EstadoController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public EstadoController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetEstado()
+        {
+            try
+            {
+                var result = _context.Estado.ToList();
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"Erro na listagem de estados. Exceção: {e.Message}");
+            }
+        }
+
+        [HttpGet("{sigla}")]
+        public IActionResult GetEstadoBySigla([FromRoute] string sigla)
+        {
+            try
+            {
+                var estado = _context.Estado.Find(sigla);
+
+                if (estado == null)
+                {
+                    return NotFound("Estado não encontrado.");
+                }
+                else
+                {
+                    return Ok(estado);
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"Erro na consulta de estados. Exceção: {e.Message}");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult PostEstado([FromBody] Estado estado)
+        {
+            try
+            {
+                _context.Estado.Add(estado);
+                var result = _context.SaveChanges();
+
+                if (result == 1)
+                {
+                    return Ok("Estado cadastrado com sucesso!");
+                }
+                else
+                {
+                    return BadRequest("Erro, estado não cadastrado.");
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"Erro, estado não incluido. Exceção: {e.Message}");
+            }
+        }
+
+        [HttpPut]
+        public IActionResult PutEstado([FromBody] Estado estado)
+        {
+            try
+            {
+                _context.Estado.Update(estado);
+                var result = _context.SaveChanges();
+
+                if (result == 1)
+                {
+                    return Ok("Estado alterado com sucesso!");
+                }
+                else
+                {
+                    return BadRequest("Erro, estado não alterado.");
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"Erro, estado não alterado. Exceção: {e.Message}");
+            }
+        }
+
+        [HttpDelete("{sigla}")]
+        public IActionResult DeleteEstado([FromRoute] string sigla)
+        {
+            try
+            {
+                var estado = _context.Estado.Find(sigla);
+                if (estado == null)
+                {
+                    return NotFound("Estado não encontrado.");
+                }
+
+                _context.Estado.Remove(estado);
+                var result = _context.SaveChanges();
+
+                if (result == 1)
+                {
+                    return Ok("Estado excluído com sucesso!");
+                }
+                else
+                {
+                    return BadRequest("Erro, estado não excluído.");
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"Erro, estado não excluído. Exceção: {e.Message}");
+            }
+        }
+    }
+}
--- WFConFin/WFConFin/obj/Debug/net8.0/WFConFin.AssemblyInfo.cs
+++ WFConFin/WFConFin/obj/Debug/net8.0/WFConFin.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WFConFin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f2bfb57128d058e1f83e35af868cb1fc5e3c42f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fb3f5df6ddabe8f20c54e31033aad2024d0f8da")]
 [assembly: System.Reflection.AssemblyProductAttribute("WFConFin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WFConFin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
